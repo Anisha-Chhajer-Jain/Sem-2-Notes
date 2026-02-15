b253782005dab06fdaf871b984da4af08b91dfba
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-15 21:11:17 +0530

    5st question

--- C++/14th feb/5.cpp
+++ C++/14th feb/5.cpp
@@ -1,42 +1,42 @@
 
 
 
-// #include <iostream>
-// #include <deque>
-// #include <string>
-// using namespace std;
+#include <iostream>
+#include <deque>
+#include <string>
+using namespace std;
 
-// int main() {
-//     deque<string> dq;
-//     string s1, s2, s3;
-//     int pos;
+int main() {
+    deque<string> dq;
+    string s1, s2, s3;
+    int pos;
 
-//     cout << "Likh 1st string-";
-//     cin >> s1;
-//     cout << "Likh 2nd string-";
-//     cin >> s2;
-//     cout << "Likh 3rd string-";
-//     cin >> s3;
+    cout << "Likh 1st string-";
+    cin >> s1;
+    cout << "Likh 2nd string-";
+    cin >> s2;
+    cout << "Likh 3rd string-";
+    cin >> s3;
 
-//     dq.push_front(s1);
+    dq.push_front(s1);
 
-//     dq.push_back(s2);
+    dq.push_back(s2);
 
-//     cout << "enter position to insert 3rd string: ";
-//     cin >> pos;
+    cout << "enter position to insert 3rd string: ";
+    cin >> pos;
 
-//     if (pos >= 0 && pos <= dq.size()) {
-//         dq.insert(dq.begin() + pos, s3);
-//     } else {
-//         cout << "Invalid position\n";
-//         dq.push_back(s3);
-//     }
+    if (pos >= 0 && pos <= dq.size()) {
+        dq.insert(dq.begin() + pos, s3);
+    } else {
+        cout << "Invalid position\n";
+        dq.push_back(s3);
+    }
 
-//     cout << "Deque wala elements:--\n";
-//     for (string str : dq) {
-//         cout << str << " ";
-//     }
+    cout << "Deque wala elements:--\n";
+    for (string str : dq) {
+        cout << str << " ";
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
