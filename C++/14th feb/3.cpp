9df8af18c2de9eccf2a8b15a9fee8231a682750a
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-15 21:10:35 +0530

    3st question

--- C++/14th feb/3.cpp
+++ C++/14th feb/3.cpp
@@ -1,25 +1,25 @@
 
 
-// // // #include <iostream>
-// // // #include <vector>
-// // // #include <algorithm>
-// // // using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-// // // int main() {
-// // //     vector<int> v;
-// // //     int x;
-// // //     cout << "Enter 5 numbers:\n";
-// // //     for (int i = 0; i < 5; i++) {
-// // //         cin >> x;
-// // //         v.push_back(x);
-// // //     }
-// // //     sort(v.begin(), v.end());
-// // //     reverse(v.begin(), v.end());// Reverse the vector
-// // //     cout << "after sorting and reversing:\n";  // Print vector elements one by one
+int main() {
+    vector<int> v;
+    int x;
+    cout << "Enter 5 numbers:\n";
+    for (int i = 0; i < 5; i++) {
+        cin >> x;
+        v.push_back(x);
+    }
+    sort(v.begin(), v.end());
+    reverse(v.begin(), v.end());// Reverse the vector
+    cout << "after sorting and reversing:\n";  // Print vector elements one by one
 
-// // //     for (int i = 0; i < v.size(); i++) {
-// // //         cout << v[i] << endl;
-// // //     }
-// // //     return 0;
-// // // }
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
+    return 0;
+}
 
