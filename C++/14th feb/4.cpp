8ceda4c23977849773cb58c53203eedcd125090d
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-15 21:10:56 +0530

    4st question

--- C++/14th feb/4.cpp
+++ C++/14th feb/4.cpp
@@ -1,53 +1,53 @@
 
-// // #include <iostream>
-// // #include <vector>
-// // #include <algorithm>
-// // using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-// // int main() {
-// //     int m, n;
-// //     cout << "Size likh L1: ";
-// //     cin >> m;
-// //     cout << "size likh L2: ";
-// //     cin >> n;
+int main() {
+    int m, n;
+    cout << "Size likh L1: ";
+    cin >> m;
+    cout << "size likh L2: ";
+    cin >> n;
 
-// //     vector<int> L1, L2;
-// //     cout << "Likh L1:\n";
-// //     for (int i = 0; i < m; i++) {
-// //         int x;
-// //         cin >> x;
-// //         L1.push_back(x);
-// //     }
+    vector<int> L1, L2;
+    cout << "Likh L1:\n";
+    for (int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+        L1.push_back(x);
+    }
 
-// //     cout << "Likh L2:\n";
-// //     for (int i = 0; i < n; i++) {
-// //         int x;
-// //         cin >> x;
-// //         L2.push_back(x);
-// //     }
+    cout << "Likh L2:\n";
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        L2.push_back(x);
+    }
     
-// //     vector<int> concat_list = L1;
-// //     concat_list.insert(concat_list.end(), L2.begin(), L2.end());
+    vector<int> concat_list = L1;
+    concat_list.insert(concat_list.end(), L2.begin(), L2.end());
 
-// //     // Sum of all elements
-// //     int total_sum = 0;
-// //     for (int num : concat_list) {
-// //         total_sum += num;
-// //     }
+    // Sum of all elements
+    int total_sum = 0;
+    for (int num : concat_list) {
+        total_sum += num;
+    }
     
-// //     sort(concat_list.begin(), concat_list.end());
-// //     cout << "Concatenated wla:--";
-// //     for (int num : concat_list) {
-// //         cout << num << " ";
-// //     }
+    sort(concat_list.begin(), concat_list.end());
+    cout << "Concatenated wla:--";
+    for (int num : concat_list) {
+        cout << num << " ";
+    }
 
-// //     cout << "\nSum:--" << total_sum << endl;
+    cout << "\nSum:--" << total_sum << endl;
 
-// //     cout << "Sorted ones:--";
-// //     for (int num : concat_list) {
-// //         cout << num << " ";
-// //     }
+    cout << "Sorted ones:--";
+    for (int num : concat_list) {
+        cout << num << " ";
+    }
 
-// //     return 0;
-// // }
+    return 0;
+}
 
