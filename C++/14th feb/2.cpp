91798b3d89d1b96eafb51d663ede1a2dd2d07b7a
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-15 21:10:01 +0530

    2st question

--- C++/14th feb/2.cpp
+++ C++/14th feb/2.cpp
@@ -1,26 +1,26 @@
-// #include <iostream>
-// #include<vector>
-// #include<algorithm>
-// using namespace std;
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
 
-// int main()
-// {
-//     vector<int> v1;
-//     int n = 5;
-//     int x = 0;
+int main()
+{
+    vector<int> v1;
+    int n = 5;
+    int x = 0;
     
-//     while(n>0){
-//         cout<< "Enter a number:  " << endl;
-//         cin >> x;
-//         v1.push_back(x);
-//         --n;
-//     }
+    while(n>0){
+        cout<< "Enter a number:  " << endl;
+        cin >> x;
+        v1.push_back(x);
+        --n;
+    }
     
-//     sort(v1.begin(), v1.end());
+    sort(v1.begin(), v1.end());
     
-//     for(int value : v1){
-//         cout << value<< endl;
-//     }
+    for(int value : v1){
+        cout << value<< endl;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
