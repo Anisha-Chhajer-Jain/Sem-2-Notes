fa3caa5e18d41517b8aed116eb7d9c3df90e54a9
Anisha Chhajer <anishachhajer16@gmail.com>
2026-02-16 18:18:30 +0530

    task performed on 14th

--- C++/STL/containers/tasks/4.cpp
+++ C++/STL/containers/tasks/4.cpp
@@ -1,39 +1,39 @@
-// // create a vector of 10 elements take input form user then erase all even element, then print the remaining 
-// // elements and totals size
+// create a vector of 10 elements take input form user then erase all even element, then print the remaining 
+// elements and totals size
 
 
-// #include <iostream>
-// #include <vector>
+#include <iostream>
+#include <vector>
 
-// using namespace std;
+using namespace std;
 
-// int main() {
-//     vector<int> v;
-//     int num;
+int main() {
+    vector<int> v;
+    int num;
 
-//     cout << "Enter 10 numbers:\n";
+    cout << "Enter 10 numbers:\n";
 
-//     for(int i = 0; i < 10; i++) {
-//         cin >> num;
-//         v.push_back(num);
-//     }
+    for(int i = 0; i < 10; i++) {
+        cin >> num;
+        v.push_back(num);
+    }
 
 
-//     for(int i = 0; i < v.size(); i++) {
-//         if(i % 2 == 0) {       
-//             v.erase(v.begin() + i);
-//             i--;                 
-//         }
-//     }
+    for(int i = 0; i < v.size(); i++) {
+        if(i % 2 == 0) {       
+            v.erase(v.begin() + i);
+            i--;                 
+        }
+    }
 
  
-//     cout << "\nRemaining elements:\n";
-//     for(int i = 0; i < v.size(); i++) {
-//         cout << v[i] << " ";
-//     }
+    cout << "\nRemaining elements:\n";
+    for(int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
 
   
-//     cout << "\nTotal size: " << v.size();
+    cout << "\nTotal size: " << v.size();
 
-//     return 0;
-// }
+    return 0;
+}
