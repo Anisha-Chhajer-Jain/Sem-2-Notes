95d121a12355263dd1b4e752cbff2e517c40ae41
Anisha Chhajer <anishachhajer16@gmail.com>
2026-03-09 15:05:42 +0530

    heirchial inheritance

---
+++ C++/2nd March/2.cpp
@@ -0,0 +1,57 @@
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// class ParentA{
+//     public:
+//         void display(){
+//             cout<<"display function for parentA"<<endl;
+//         }
+// };
+
+
+
+
+// class ChildA: virtual public ParentA{
+//     public:
+//         void displayA(){
+//             cout<<"display fn for childA"<<endl;
+//         }
+// };
+
+// class ChildB: virtual public ParentA{
+//     public:
+//         void displayB(){
+//             cout<<"display fn for childB"<<endl;
+//         }
+// };
+
+// class ChildC: public ChildA, public ChildB{
+//         public:
+//         void displayC(){
+//             cout<<"display fn for childC"<<endl;
+//         }
+// };
+
+
+
+// int main()
+// {
+//     ChildC c1;
+//     cout<<"the parentA function is called through child C "<<endl;
+//     c1.display();  //inerited from parent1             //virtual inheritance
+//     c1.ChildA::display();   //scope resolution
+   
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
